93608adfc36826a4948f9b66f3132c24eb24ad6b
gayhf <gayhf.com@gmail.com>
2022-05-18 10:38:48 +0700

    Update mi.js

--- mi.js
+++ mi.js
@@ -1,1 +1,1 @@
-
+if (cia=="00"){document.write('<table bgcolor="#da8688" cellpadding="1" cellspacing="0" valign="top"><tbody><tr><td><table bgcolor="#f7e5e6" cellpadding="3" cellspacing="0" width=553 valign="top"><tbody><tr align="center"><td><b>Size</b>: ',size,' | <b>Format</b>: ',format,' | <b>Resolution</b>: ',resolution,'  | <b>Framerate</b>: ',frame,' fps <b>',sub,'</b><br/><a href="https://support.gayhf.com/p/how-to-download.html" target="_blank">Click here to know How to Download!</a></td></tr></tbody></table></td></tr></tbody></table>');};
